d9b6cd5eecd4ec75a729fc734808387b5208544f
andyque <guanghui8827@126.com>
2012-09-14 09:18:39 +0800

    Initial Commit

---
+++ VideoPanoramaMosaics/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  VideoPanoramaMosaics
+//
+//  Created by guanghui on 9/14/12.
+//  Copyright (c) 2012 guanghui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
